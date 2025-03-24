089f0eec36b29aba1790842314dbd7c6ba59404a
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-03-24 23:46:25 +0530

    Add Notification, Payment, Review, and Ticket models with schemas for event management

---
+++ Backend/models/Notifications.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
