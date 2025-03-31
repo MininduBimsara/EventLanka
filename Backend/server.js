a6aae6638a704fe71f1c5ed1f7ab41125d618d6f
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-03-31 20:29:23 +0530

    Add session management and passport integration for enhanced authentication

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,10 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./Config/db");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const passportConfig = require("./Config/passportConfig");
+
 dotenv.config();
 connectDB();
 
@@ -13,15 +17,28 @@ app.use(cors());
 
 app.use(cookieParser());
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Routes
 const ticketRoutes = require("./routes/ticketRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const authRoutes = require("./routes/authRoutes");
+const googleAuthRoutes = require("./routes/googleAuthroutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/tickets", ticketRoutes);
+app.use("/api/googleauth", googleAuthRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
