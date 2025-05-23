322145ebe79cd440c2d81005f30829a12e3e7f39
Minindu Bimsara Abeywardena <146797344+MininduBimsara@users.noreply.github.com>
2025-05-24 01:22:33 +0530

    Update OrganizerValidationMiddleware.js

--- Backend/middleware/OrganizerValidationMiddleware.js
+++ Backend/middleware/OrganizerValidationMiddleware.js
@@ -1,17 +1,6 @@
 const { body, param, validationResult } = require("express-validator");
 
-// Validation middleware
-const validate = (validations) => {
-  return async (req, res, next) => {
-    await Promise.all(validations.map((validation) => validation.run(req)));
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  };
-};
 
 // Validation rules for various endpoints
 const attendeeValidations = {
