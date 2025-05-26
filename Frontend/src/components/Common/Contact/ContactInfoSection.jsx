d65087dcf095ed4a2b4b10c64d0dc38c292da62e
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-05-27 01:21:55 +0530

    Update contact information in ContactInfoSection and Footer components to reflect new email and address details

--- Frontend/src/components/Common/Contact/ContactInfoSection.jsx
+++ Frontend/src/components/Common/Contact/ContactInfoSection.jsx
@@ -38,7 +38,7 @@ const ContactInfoSection = () => {
                 href="mailto:hello@ticketmagic.com"
                 className="text-lg font-medium"
               >
-                hello@ticketmagic.com
+                info@EventLanka.com
               </a>
             </div>
           </div>
@@ -50,7 +50,7 @@ const ContactInfoSection = () => {
             <div>
               <div className="text-sm opacity-70">Office Address</div>
               <address className="text-lg not-italic font-medium">
-                123 Event Street, Party City, PC 12345
+                123 Chatham Street, Colombo 1, Sri Lanka
               </address>
             </div>
           </div>
@@ -78,7 +78,7 @@ const ContactInfoSection = () => {
             >
               <Facebook />
             </a>
-            <a
+            {/* <a
               href="#"
               className="flex items-center justify-center w-12 h-12 text-[#2a2e60] transition-all duration-300 transform bg-white rounded-full bg-opacity-10 hover:scale-110 hover:bg-black hover:rotate-6"
             >
@@ -97,31 +97,30 @@ const ContactInfoSection = () => {
                 <path d="M12.5 9.5c.5-1.5 1.5-2 2.5-2 1.5 0 2.5 1.5 2.5 2.5 0 1.5-2.5 2.5-3 3 1 1.5 3 1.5 4 2"></path>
                 <path d="M10.5 12c-1.5 1.5-2 3.5-2 5.5 0 2 1 3 3.5 3s3.5-1 3.5-3c0-2-2-3.5-2-5.5"></path>
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
 
       {/* Map Section */}
       <div className="overflow-hidden border-4 border-white shadow-lg rounded-2xl border-opacity-20">
-        <div className="relative h-64 pt-0 pb-0 bg-gray-200">
-          <div className="absolute inset-0 flex items-center justify-center bg-purple-900 bg-opacity-20">
+        <div className="relative pt-0 pb-0 bg-gray-200">
+          {/* <div className="flex items-center justify-center py-4 bg-purple-900 bg-opacity-20">
             <div className="flex items-center p-4 bg-white rounded-lg shadow-lg bg-opacity-90">
               <MapPin className="mr-2 text-purple-600" />
               <span className="font-medium">Our Office Location</span>
             </div>
-            <div className="absolute p-2 bg-white rounded-lg shadow-lg bottom-4 right-4">
-              <img
-                src="/api/placeholder/100/30"
-                alt="Map controls placeholder"
-              />
-            </div>
-          </div>
-          <img
-            src="/api/placeholder/600/300"
-            alt="Location Map"
-            className="object-cover w-full h-full"
-          />
+          </div> */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.6309612219507!2d79.8439707!3d6.9346346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259242dcf61e5%3A0xdbeb4ebb15d9a12!2s83%201%2F1%20Chatham%20St%2C%20Colombo%2000100!5e0!3m2!1sen!2slk!4v1748281689329!5m2!1sen!2slk"
+            width="100%"
+            height="350"
+            style={{ border: 0, display: "block" }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Map controls"
+          ></iframe>
         </div>
       </div>
     </div>
